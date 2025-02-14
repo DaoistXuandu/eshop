e97138294de81af39f910dc94ce7b46e0d796896
DaoistXuandu <akbarraihan2005@gmail.com>
2025-02-14 17:38:36 +0700

    fix repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -3,47 +3,16 @@ package id.ac.ui.cs.advprog.eshop.repository;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
+
     public Product create(Product product) {
-        product.setProductId(productData.size() + "0");
         productData.add(product);
-        return product;
-    }
-
-    public Product findById(String productId) {
-        for (Product product : productData) {
-            if (product.getProductId().equals(productId)) {
-                return product;
-            }
-        }
-        return null;
-    }
-
-    public Product edit(Product product) {
-        for (int i = 0; i < productData.size(); i++) {
-            if (productData.get(i).getProductId().equals(product.getProductId())) {
-                productData.set(i, product);
-                return product;
-            }
-        }
-        return null;
-    }
-
-    public Product delete(String productId) {
-        Product product = null;
-        for(int i = 0; i < productData.size(); i++){
-            if(productData.get(i).getProductId().equals(productId)){
-                product = productData.get(i);
-            }
-        }
-
-        productData.remove(product);
         return product;
     }
 
