a5f0d48eeea3990ccbb3008a92a586afd36ec12b
DaoistXuandu <akbarraihan2005@gmail.com>
2025-03-07 16:24:53 +0700

    [GREEN] add imple for paymentrepository

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -0,0 +1,35 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import org.springframework.stereotype.Repository;
+import id.ac.ui.cs.advprog.eshop.model.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class PaymentRepository {
+    private List<Payment> payments = new ArrayList<>();
+    public Payment save(Payment payment) {
+        int i = 0;
+        for (Payment paymentData : payments) {
+            if (paymentData.getId().equals(payment.getId())) {
+                payments.remove(i);
+                payments.add(i, payment);
+                return payment;
+            }
+            i += 1;
+        }
+
+        payments.add(payment);
+        return payment;
+    }
+
+    public Payment findById(String id) {
+        for (Payment savedPayment : payments) {
+            if (savedPayment.getId().equals(id)) {
+                return savedPayment;
+            }
+        }
+        return null;
+    }
+}
