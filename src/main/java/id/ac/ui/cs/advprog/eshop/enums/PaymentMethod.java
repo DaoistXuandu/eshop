bb8ca0dc66e149e558d6fdc2a7dd88408c5ba7d0
DaoistXuandu <akbarraihan2005@gmail.com>
2025-03-07 16:42:55 +0700

    [REFACTOR] adding new enum

--- src/main/java/id/ac/ui/cs/advprog/eshop/enums/PaymentMethod.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/enums/PaymentMethod.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum PaymentMethod {
     BANK_TRANSFER("BANK_TRANSFER"),
-    VOUCHER_CODE("VOUCHER_CODE");
+    VOUCHER_CODE("VOUCHER_CODE"),
+    PENDING("PENDING");
 
     private final String value;
     private PaymentMethod(String value) {
