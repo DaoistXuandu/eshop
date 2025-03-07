454aa74c49be8ddba28f2fcc5122f9f871f54b10
DaoistXuandu <akbarraihan2005@gmail.com>
2025-03-07 13:05:52 +0700

    [RED] Add Order model skeleton.

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -0,0 +1,28 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import java.util.Arrays;
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+public class Order {
+    String id;
+    List<Product> products;
+    long orderTime;
+    String author;
+    @Setter
+    String status;
+
+    public Order(String id, List<Product> products, long orderTime, String author) {
+    }
+
+    public Order(String id, List<Product> products, Long orderTime, String author, String status) {
+    }
+
+    public void setStatus(String status) {
+    }
+}
