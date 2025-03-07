6f33cf8c95c0b05ae38613f28b925ab34812d264
DaoistXuandu <akbarraihan2005@gmail.com>
2025-03-07 15:55:57 +0700

    fix: OrderServiceTest file misplaced folder

--- src/test/java/id/ac/ui/cs/advprog/eshop/productService/OrderServiceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/productService/OrderServiceTest.java
@@ -1,9 +1,10 @@
-package id.ac.ui.cs.advprog.eshop.service;
+package id.ac.ui.cs.advprog.eshop.productService;
 
 import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import id.ac.ui.cs.advprog.eshop.repository.OrderRepository;
+import id.ac.ui.cs.advprog.eshop.service.OrderServiceImpl;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
